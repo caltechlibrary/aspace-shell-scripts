931eb13fea30703400efedacfe56407231a79fe7
R. S. Doiel <rsdoiel@gmail.com>
2015-12-18 10:52:18 -0800

    snapshot

--- api-logout.sh
+++ api-logout.sh
@@ -3,7 +3,7 @@
 # Sanity check
 if [ "$ASPACE_API_URL" != "" ] && [ "$ASPACE_API_TOKEN" != "" ]; then
     echo "Sending logging out with $ASPACE_API_URL/logout"
-    curl -H "X-ArchivesSpace-Session: $ASPACE_API_TOKEN" $ASPACE_API_URL/logout
+    curl -H "X-ArchivesSpace-Session: $ASPACE_API_TOKEN" $ASPACE_API_URL/logout > /dev/null
 fi
 
 export ASPACE_API_URL=""
